e167cc76f3ab5496031d9ae78ecb5e2f7738de66
Raeed-Ahmed-Audri <audrothewise@gmail.com>
2024-03-16 23:29:52 -0600

    Commit 2

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,12 @@
-// Call this function whenever you want to fetch and display new data
-function fetchData() {
-    var selectedBuilding = $('#buildingFilter').val();
-    google.script.run.withSuccessHandler(function(data) {
-        displayData(data, selectedBuilding);
-    }).getAllBuildingsData();
-  }
-  
-  function displayData(data, selectedBuilding) {
-    var container = $('#dataContainer');
-    container.empty(); // Clear previous data
-    var filteredData = data;
-    if(selectedBuilding !== 'all') {
-        filteredData = data.filter(item => item.building === selectedBuilding);
+$(document).ready(function(){
+  $("[data-fancybox]").fancybox();
+  $(".item").isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
     }
-  
-    filteredData.forEach(function(item) {
-        var para = $('<p>').text(`Building: ${item.building}, Room: ${item.room}, Utilization: ${item.utilization}%`);
-        container.append(para);
-    });
-  }
-  
-  // Initial fetch and display
-  $(document).ready(function() {
-    fetchData();
-  });
+  })
+});
   
