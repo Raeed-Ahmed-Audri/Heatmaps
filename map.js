8c9220b64b3caa1234d10a1d8344fff5555c6272
Engg User <Engg-User@LT-743399.engineering.ualberta.ca>
2024-03-20 12:12:08 -0600

    Updated map size to match image size, and legend created.

--- map.js
+++ map.js
@@ -1,42 +1,54 @@
 function mapLoader(divName, rooms, roomPoints, utilNums, imagePath, imageSize) {
-    var colors = ["blue", "green", "yellow", "orange", "red"];
-//Choose colours based on utilization
-var colorMap = new Map();
-for (i = 0; i < rooms.length; i++) {
-    if (utilNums[i] >= 0.8) {
-        colorMap.set(rooms[i], colors[4]);
-    } else if (utilNums[i] < 0.8 && utilNums[i] >= 0.6) {
-        colorMap.set(rooms[i], colors[3]);
-    } else if (utilNums[i] < 0.6 && utilNums[i] >= 0.4) {
-        colorMap.set(rooms[i], colors[2]);
-    } else if (utilNums[i] < 0.4 && utilNums[i] >= 0.2) {
-        colorMap.set(rooms[i], colors[1]);
-    } else {
-        colorMap.set(rooms[i], colors[0]);
+    function getColour(value) {
+        return value < 0.2 ? 'blue' :
+            value < 0.4 ? 'green' :
+                value < 0.6 ? 'yellow' :
+                    value < 0.8 ? 'orange' :
+                        value < 1 ? 'red' :
+                            'purple';
     }
-}
-//Create map object in div named CME7
-var map = L.map(divName, {
-    crs: L.CRS.Simple,
-    minZoom: -1.5
-}).setView([0, 0], 0);
-//Set floorplan as the main tile of the map
-var bounds = [[0, 0], imageSize];
-var image = L.imageOverlay(imagePath, bounds).addTo(map);
-map.fitBounds(bounds);
-//load each room overlay
-for (j = 0; j < rooms.length; j++) {
-    var room = L.polygon(roomPoints[j], {
-        color: colorMap.get(rooms[j]),
-        fillColor: colorMap.get(rooms[j]),
-        fillOpacity: 0.5
-    }).addTo(map);
-    room.bindPopup("<b>" + rooms[j] + "</b><br>Utilization: " + (100 * utilNums[j]) + "%");
-}
-//Set marker with coordinates when a spot is clicked
-let marker = null;
-map.on('click', (event) => {
-    marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
-    marker.bindPopup("Coords (lat/lng): " + event.latlng.lat + ", " + event.latlng.lng);
-})
+    //Choose colours based on utilization
+    var colorMap = new Map();
+    for (i = 0; i < rooms.length; i++) {
+        colorMap.set(rooms[i], getColour(utilNums[i]))
+    }
+    //Create map object in div named CME7
+    var map = L.map(divName, {
+        crs: L.CRS.Simple,
+        minZoom: -1.5
+    }).setView([0, 0], 0);
+    //Set floorplan as the main tile of the map
+    var bounds = [[0, 0], imageSize];
+    L.imageOverlay(imagePath, bounds).addTo(map);
+    map.fitBounds(bounds);
+    //load each room overlay
+    for (j = 0; j < rooms.length; j++) {
+        var room = L.polygon(roomPoints[j], {
+            color: colorMap.get(rooms[j]),
+            fillColor: colorMap.get(rooms[j]),
+            fillOpacity: 0.5
+        }).addTo(map);
+        room.bindPopup("<b>" + rooms[j] + "</b><br>Utilization: " + (utilNums[j] / 0.01) + "%");
+    }
+    //Set marker with coordinates when a spot is clicked
+    let marker = null;
+    map.on('click', (event) => {
+        marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
+        marker.bindPopup("Coords (lat/lng): " + event.latlng.lat + ", " + event.latlng.lng);
+    })
+    //Generate legend and add to map
+    var legend = L.control({ position: 'bottomright' });
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 0.2, 0.4, 0.6, 0.8, 1];
+
+        //Generate legend item for each color region
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColour(grades[i]) + '"></i> ' + 100 * grades[i] + (grades[i + 1] ? '&ndash;' + 100 * grades[i + 1] + '%<br>' : '+%');
+        }
+        return div;
+    };
+    legend.addTo(map);
 }
